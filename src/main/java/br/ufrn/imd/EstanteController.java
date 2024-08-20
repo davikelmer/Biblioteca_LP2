0b8acc69e70d7215c4d2c14b13198825977d8f41
Davi Kelmer <davikms9@gmail.com>
2024-08-20 10:41:50 -0300

    Fix: listarLivros e carregarEstante

--- src/main/java/br/ufrn/imd/EstanteController.java
+++ src/main/java/br/ufrn/imd/EstanteController.java
@@ -5,10 +5,12 @@ import java.util.Scanner;
 import static br.ufrn.imd.Estante.estante;
 
 public class EstanteController {
+
     public void listarLivros(){
         for(Livro livro : estante) {
             System.out.println(livro);
         }
+
     }
 
 
